e792d13e002ee9c77f3c5054324953577ebaaa9c
TheYgDev <Giyuval0@gmail.com>
2023-09-04 12:31:00 +0300

    first commit

---
+++ chat-app/src/pages/dashboard/GeneralApp.js
@@ -0,0 +1,14 @@
+import  Chats  from "./Chats";
+import React from "react";
+
+const GeneralApp = () => {
+
+  return (
+    <>
+      
+      <Chats/>
+    </>
+  );
+};
+
+export default GeneralApp;
